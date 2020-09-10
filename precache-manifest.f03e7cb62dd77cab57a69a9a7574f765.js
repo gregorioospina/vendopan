327b8be6d45bb458b0fcb5395190a1ff34a52977
gregorioospina <gregorioospina@hotmail.com>
2020-09-10 15:46:53 -0500

    Updates

---
+++ precache-manifest.f03e7cb62dd77cab57a69a9a7574f765.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdc6c8d9964bc3121e9d0e1419b8346",
+    "url": "/vendopan/index.html"
+  },
+  {
+    "revision": "90d6502d71b035ba46a4",
+    "url": "/vendopan/static/css/main.a7d7fec3.chunk.css"
+  },
+  {
+    "revision": "7fc36223e45149565ff7",
+    "url": "/vendopan/static/js/2.060eb7c8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/vendopan/static/js/2.060eb7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90d6502d71b035ba46a4",
+    "url": "/vendopan/static/js/main.f79b9e03.chunk.js"
+  },
+  {
+    "revision": "689c9d995c0e63324aa4",
+    "url": "/vendopan/static/js/runtime-main.63283a2b.js"
+  },
+  {
+    "revision": "6e2c8f8a3737771d7bcbb87658b4b3d8",
+    "url": "/vendopan/static/media/fondo.6e2c8f8a.png"
+  },
+  {
+    "revision": "2c4f09ce9788154e32f40eb5c98e86ba",
+    "url": "/vendopan/static/media/pan.2c4f09ce.png"
+  }
+]);
